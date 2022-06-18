96c78c21795cb52dc1d87178885cf01b3e383c1f
Kuznetsov Yegor <kuznetsovyegor25@gmail.com>
2022-06-19 01:27:41 +0300

    added mobile device detect condition

--- unit1.5/scripts/swiper.js
+++ unit1.5/scripts/swiper.js
@@ -1,19 +1,25 @@
-new Swiper('.repair-brands__swiper-container', {
-   
-   pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-   }, 
+{
+   let isMobileDevice = window.matchMedia(
+      "(max-width: 724px) and (min-resolution: 190dpi)"
+   ).matches;
 
-   slideToClickedSlide: true, 
+   if (isMobileDevice) {
+      new Swiper(".repair-brands__swiper-container", {
+         pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+         },
 
-   autoWidth: true,
+         slideToClickedSlide: true,
 
-   slidesPerView: 'auto',
-});
+         autoWidth: true,
 
+         slidesPerView: "auto",
+      });
+   }
+}
 
-
+// "only screen and (max-width: 760px)"
 
 // if (typeof screen.orientation == 'undefined') {
 //    document.querySelector('.swiper-wrapper').classList.remove('swiper-wrapper');
