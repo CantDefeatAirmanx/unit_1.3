1795f359f10742e99c6b0be001ae2e296489f1e2
Kuznetsov Yegor <kuznetsovyegor25@gmail.com>
2022-06-19 03:25:15 +0300

    update of show/close btn

--- unit1.5/scripts/show-all-btn.js
+++ unit1.5/scripts/show-all-btn.js
@@ -1,16 +1,19 @@
 {
    let brandsList = document.querySelector(".repair-brands__list");
    let listHandlerBtn = document.querySelector(".repair-brands__showbtn");
+   let listHandlerBtnText = listHandlerBtn.querySelector('.repair-brands__showbtn-text');
 
    listHandlerBtn.addEventListener("click", function () {
       if (brandsList.classList.contains("repair-brands__list--opened")) {
          brandsList.classList.remove("repair-brands__list--opened");
          brandsList.classList.add("repair-brands__list--closed");
+         listHandlerBtnText.textContent = 'Показать все';
       } 
 
       else if (brandsList.classList.contains("repair-brands__list--closed")) {
          brandsList.classList.remove("repair-brands__list--closed");
          brandsList.classList.add("repair-brands__list--opened");
+         listHandlerBtnText.textContent = 'Скрыть';
       }
    });
 }
